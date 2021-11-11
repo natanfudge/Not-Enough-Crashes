505703c7b336553b750a637a3b7112e6e68ea190
Fudge <natan.lifsiz@gmail.com>
2021-11-11 23:06:25 +0200

    support loader 0.12+ (#92)

--- fabric/src/main/java/fudge/notenoughcrashes/fabric/mixinhandlers/ModLoaders.java
+++ fabric/src/main/java/fudge/notenoughcrashes/fabric/mixinhandlers/ModLoaders.java
@@ -1,12 +1,12 @@
 package fudge.notenoughcrashes.fabric.mixinhandlers;
 
-import net.fabricmc.loader.entrypoint.minecraft.hooks.EntrypointClient;
+import net.fabricmc.loader.impl.game.minecraft.Hooks;
 
 import java.io.File;
 
 public class ModLoaders {
     public static void fabricEntrypoints(File runDir, Object gameInstance) {
-        EntrypointClient.start(runDir, gameInstance);
+        Hooks.startClient(runDir, gameInstance);
     }
     public static void quiltEntrypoints(File runDir, Object gameInstance) {
         org.quiltmc.loader.impl.entrypoint.minecraft.hooks.EntrypointClient.start(runDir, gameInstance);
