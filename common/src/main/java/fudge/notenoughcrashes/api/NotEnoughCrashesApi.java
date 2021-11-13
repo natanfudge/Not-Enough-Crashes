24f89833d6862f643ca93ccf777a43daeca01d16
natanfudge <natan.lifsiz@gmail.com>
2021-11-13 23:44:05 +0200

    Simple Cleanup

--- common/src/main/java/fudge/notenoughcrashes/api/NotEnoughCrashesApi.java
+++ common/src/main/java/fudge/notenoughcrashes/api/NotEnoughCrashesApi.java
@@ -1,18 +1,18 @@
-package fudge.notenoughcrashes.api;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class NotEnoughCrashesApi {
-    public static final List<Runnable> permanentDisposers = new ArrayList<>();
-    public static final List<Runnable> oneTimeDisposers = new ArrayList<>();
-
-    public static void onEveryCrash(Runnable disposer){
-        permanentDisposers.add(disposer);
-    }
-
-    public static void onNextCrash(Runnable disposer) {
-        oneTimeDisposers.add(disposer);
-    }
-}
-
+//package fudge.notenoughcrashes.api;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class NotEnoughCrashesApi {
+//    public static final List<Runnable> permanentDisposers = new ArrayList<>();
+//    public static final List<Runnable> oneTimeDisposers = new ArrayList<>();
+//
+//    public static void onEveryCrash(Runnable disposer){
+//        permanentDisposers.add(disposer);
+//    }
+//
+//    public static void onNextCrash(Runnable disposer) {
+//        oneTimeDisposers.add(disposer);
+//    }
+//}
+//
--- common/src/main/java/fudge/notenoughcrashes/mixins/MixinCrashReport.java
+++ common/src/main/java/fudge/notenoughcrashes/mixins/MixinCrashReport.java
@@ -1,10 +1,8 @@
 package fudge.notenoughcrashes.mixins;
 
-import fudge.notenoughcrashes.platform.CommonModMetadata;
 import fudge.notenoughcrashes.stacktrace.ModIdentifier;
 import net.minecraft.util.SystemDetails;
 import net.minecraft.util.crash.CrashReport;
-import net.minecraft.util.crash.CrashReportSection;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,55 +11,19 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 
-@SuppressWarnings("unused")
 @Mixin(value = CrashReport.class, priority = 500)
-public abstract class MixinCrashReport /*implements PatchedCrashReport*/ {
+public abstract class MixinCrashReport {
 
     @Shadow
     @Final
     private SystemDetails systemDetailsSection;
-    @Shadow
-    @Final
-    private List<CrashReportSection> otherSections;
-    @Shadow
-    @Final
-    private Throwable cause;
-    @Shadow
-    @Final
-    private String message;
-
-
-    private Set<CommonModMetadata> suspectedMods;
-
-    @Shadow
-    private static String generateWittyComment() {
-        return null;
-    }
-
-    private static String stacktraceToString(Throwable cause) {
-        StringWriter writer = new StringWriter();
-        cause.printStackTrace(new PrintWriter(writer));
-        return writer.toString();
-    }
 
     private CrashReport getThis() {
         return (CrashReport) (Object) this;
     }
-
-//    @Override
-//    @NotNull
-//    public Set<CommonModMetadata> getSuspectedMods() {
-//        if (suspectedMods == null) suspectedMods = ModIdentifier.identifyFromStacktrace(cause);
-//        return suspectedMods;
-//    }
-
 
     /**
      * @reason Adds a list of mods which may have caused the crash to the report.
@@ -82,5 +44,3 @@ public abstract class MixinCrashReport /*implements PatchedCrashReport*/ {
         });
     }
 }
-
-//                            mods.joinToString(", ") {mod -> "${mod.name} (${mod.id})"
