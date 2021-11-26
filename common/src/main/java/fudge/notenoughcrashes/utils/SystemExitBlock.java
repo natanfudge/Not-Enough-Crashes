6905e0df0cd281127665c40be86b72b0e83928f6
Fudge <natan.lifsiz@gmail.com>
2021-11-26 20:25:46 +0200

    1.18-rc3

--- common/src/main/java/fudge/notenoughcrashes/utils/SystemExitBlock.java
+++ common/src/main/java/fudge/notenoughcrashes/utils/SystemExitBlock.java
@@ -7,13 +7,13 @@ import java.security.Permission;
 public class SystemExitBlock {
     public static void forbidSystemExitCall() {
         final SecurityManager securityManager = new SecurityManager() {
-            public void checkPermission( Permission permission ) {
-                if(NecConfig.instance().forceCrashScreen && permission.getName().startsWith("exitVM")  ) {
+            public void checkPermission(Permission permission) {
+                if (NecConfig.instance().forceCrashScreen && permission.getName().startsWith("exitVM")) {
                     throw new SystemExitBlockedException("An attempt was made to forcefully close the game with no stack trace (see stack trace)." +
-                            " Not Enough Crashes made the game simply crash instead since the forceCrashScreen option is enabled.") ;
+                            " Not Enough Crashes made the game simply crash instead since the forceCrashScreen option is enabled.");
                 }
             }
-        } ;
-        System.setSecurityManager( securityManager ) ;
+        };
+        System.setSecurityManager(securityManager);
     }
 }
