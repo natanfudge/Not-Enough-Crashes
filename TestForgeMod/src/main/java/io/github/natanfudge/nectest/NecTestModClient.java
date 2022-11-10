053ec5de153fb18609f33290313d990c799e9f59
sschr15 <steven.sschr15@hotmail.com>
2022-11-09 18:54:59 -0600

    Update Forge test mod to 1.19.2

--- TestForgeMod/src/main/java/io/github/natanfudge/nectest/NecTestModClient.java
+++ TestForgeMod/src/main/java/io/github/natanfudge/nectest/NecTestModClient.java
@@ -1,15 +1,13 @@
 package io.github.natanfudge.nectest;
 
 import net.minecraft.client.option.KeyBinding;
-import net.minecraftforge.client.ClientRegistry;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import org.lwjgl.glfw.GLFW;
 
 public class NecTestModClient {
     public static final KeyBinding key = new KeyBinding("key.nec_test.crash", GLFW.GLFW_KEY_LEFT_BRACKET, "category.nec_test");
 
-
-    public static void clientSetup(final FMLClientSetupEvent event) {
-        ClientRegistry.registerKeyBinding(key);
+    public static void onKeyRegister(RegisterKeyMappingsEvent event) {
+        event.register(key);
     }
 }
